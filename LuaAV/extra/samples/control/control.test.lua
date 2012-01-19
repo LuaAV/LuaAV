573a1bfedb9af79a3d20b6c035d7c662aed1674b
thecharlie <charlie@charlie-roberts.com>
2012-01-18 17:10:13 -0800

    control module test script changes

--- LuaAV/extra/samples/control/control.test.lua
+++ LuaAV/extra/samples/control/control.test.lua
@@ -1,61 +1,40 @@
-
-
 local control = require "control"
 local Widget = control.Widget
 
 -- this will be optional soon, once bonjour etc. is working:
+--[[
 control.init{
-	name = "LuaAV",
-	orientation = "portrait",
+	--name = "LuaAV",
+	--orientation = "portrait",
 	
-	host = "10.0.2.4",
-	port = 8081,
+	--host = "127.0.0.1",
+	--port = 8081,
 	
-	remote_host = "10.0.2.1",
-	remote_port = 8080
+	--remote_host = "127.0.0.1",
+	--remote_port = 8080
 }
+--]]
 
 -- add widgets
+wait(1)
 Widget{} -- default type Slider
-Widget{ type="Button", name="Bang", min=1, max=2, }
-Widget{ type="Knob", name="Twiddler", }
-Widget{ type="Accelerometer" }
+local button = Widget{ type="Button", min=1, max=2, }
+
+function twiddle(widget)
+    print(widget, widget.name, widget.value)
+end
+
+Widget{ type="Knob", name="Twiddler", callback=twiddle }
 
 wait(1)
 
 -- send a value:
-control.set("Bang", 2)
+button.set(2)
+
+control.set("Twiddler", 0.5)
+
+wait(1)
 
 control.remove("Twiddler")
 
 
-
--- install a generic handler:
-control.handler = function(m)
-	print(m.addr, m.types, unpack(m))
-end
-
-
---[=[
-
-
-go(function()
-	while true do
-		--send:send("/Slider1", math.random())
-		--send:send("/Knobbersmith", math.random())
-		wait(0.5)
-	end
-end)
-
--- receive controls:
-while true do
-	for m in recv:recv() do
-		print(m.addr, m.types, unpack(m))
-		local slidername = m.addr
-		local value = m[1]
-	end
-	wait(0.1)
-end
-
-
---]=]
