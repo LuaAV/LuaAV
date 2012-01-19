886f4cd77525bcf1c7e40bde5d615c7cd441fcfa
thecharlie <charlie@charlie-roberts.com>
2012-01-18 17:09:33 -0800

    polished control module / test script

--- LuaAV/extra/modules/control/init.lua
+++ LuaAV/extra/modules/control/init.lua
@@ -1,12 +1,5 @@
 local osc = require "osc"
-
 local format = string.format
-
--- some selfaults:
-local remote_host_default = "localhost" --"192.168.1.124"
-local remote_port_default = 8080
-local myip = "localhost"
-local myport = 8081
 
 local uid = (function()
 	local id = 0
@@ -29,9 +22,9 @@ local Master_meta = {
 	name = uid("LuaAV"),
 	
 	-- defaults:
-	remote_host = "localhost",
+	remote_host = "127.0.0.1",
 	remote_port = 8080,
-	host = "localhost",
+	host = "127.0.0.1",
 	port = 8081,
 	shouldDisplayMenu = 1,
 	orientation = "landscape",
@@ -39,14 +32,6 @@ local Master_meta = {
 	handler = function() end,
 }
 Master_meta.__index = Master_meta
-
-function Master_meta:send(address, ...)
-	self.oscsend:send(address, ...)
-end
-
-function Master_meta:send(address, ...)
-	self.oscsend:send(address, ...)
-end
 
 local self = {
 	_widgets = {}
@@ -64,7 +49,7 @@ self.init = function(def)
 	
 	-- fix parameter types:
 	-- FIXME: many more tests here
-	self.shouldDisplayMenu = self.shouldDisplayMenu and (self.shouldDisplayMenu ~= 0) or 0
+	self.shouldDisplayMenu = self.shouldDisplayMenu and (self.shouldDisplayMenu ~= 0 and 1 or 0) or 0
 	
 	-- close sockets:
 	self.oscsend = nil
@@ -77,36 +62,53 @@ self.init = function(def)
 	self.oscrecv = osc.Recv(self.port)
 	
 	-- handshake:
-	self:send("/control/createBlankInterface", 
+	self.send("/control/createBlankInterface", 
 		self.name, self.orientation, self.shouldDisplayMenu
 	)
-	self:send("/control/pushDestination", 
+	self.send("/control/pushDestination", 
 		string.format("%s:%d", self.host, self.port)
 	)
 end
 
+
+function self.send(address, ...)
+	self.oscsend:send(address, ...)
+end
+
+local function printer(w) print("asdasdasd", w.name, w.value) end
+
 self.Widget = function(def)	
 	def.type = def.type or "Slider"
 	def.name = def.name or uid(def.type)
+	def.callback = def.callback or printer
+	def.set = function(v)
+	    def.value = v
+	    self.send("/"..def.name, v)
+    end
+	
 	local json = makeJSON(def)
 	local options = [[{ \"min\" : 1, \"max" : 6, }]]
 	--print(json)
-	self:send("/control/addWidget", json) --, options)
+	self.send("/control/addWidget", json) --, options)
 	
-	self._widgets[def.name] = def
-end
-
-self.set = function(name, ...)
-	self:send(format("/%s", name), ...)
+	self._widgets["/"..def.name] = def
+	return def -- should return def???
 end
 
 self.remove = function(name)
-	self:send("/control/removeWidget", name)
-	self._widgets[name] = nil
+	self.send("/control/removeWidget", name)
+	self._widgets["/"..name] = nil
 end
 
 self.recv = function()
 	return self.oscrecv:recv()
+end
+
+self.set = function(addr, v)
+    local w = self._widgets["/"..addr]
+    if w then 
+	    w.set(v)
+	end
 end
 
 -- create default interface:
@@ -116,12 +118,26 @@ self.init{}
 go(function()
 	while self do
 		for m in self.oscrecv:recv() do
-			
+		    print(m.addr, m.types, unpack(m))
+		    -- find widget in map:
+		    local w = self._widgets[m.addr]
+		    if w then
+		        w.value = m[1]
+		        w.values = m
+		        if w.callback then
+		        local ok, err = pcall(w.callback, w)
+    		        if not ok then
+    				    print(format("control error (%s): %s", m.addr, err))  
+    	            end
+    		    end
+			end
 			-- delegate to user-handler:
+			--[[
 			local ok, err = pcall(self.handler, m)
 			if not ok then 
 				print(format("control error (%s): %s", m.addr, err))
 			end
+			--]]
 		end
 		wait(self.poll_interval or 0.1)
 	end
